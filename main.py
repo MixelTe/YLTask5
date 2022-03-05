087db809ddbbc849e2f67ecdc8aa6f714db9692d
Mike <63655075+MixelTe@users.noreply.github.com>
2022-03-05 20:39:14 +0300

    Authorization

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from flask import Flask, redirect, render_template
+from flask_login import LoginManager, login_required, login_user, logout_user
 from data import db_session
 from data.jobs import Jobs
 from data.users import User
+from forms.login import LoginForm
 from forms.register import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 def main():
     db_session.global_init("db/mars_explorer.db")
     app.run()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 @app.route("/")
@@ -44,6 +54,28 @@ def register():
 @app.route("/register_success")
 def register_success():
     return render_template('/register_success.html', title='Registration')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html', title='Авторизация',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 if __name__ == '__main__':
